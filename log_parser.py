fef5b02a6c77d80c382e4b69e6cf0c0c2b314585
Neil Tucker <49943805+neiltucker@users.noreply.github.com>
2026-01-26 18:20:55 -0500

    Update log_parser.py

--- log_parser.py
+++ log_parser.py
@@ -90,22 +90,22 @@ def version_compare_simple(version_string):
 def calculate_network_status(process_block_seconds_ago):
     """
     Calculate network status based on time since last process_block
-    < 120 seconds: Optimal
-    2-5 minutes: Normal
-    5-10 minutes: Delayed
-    10-15 minutes: Degraded
-    > 15 minutes: Offline
+    < 5 minutes: Optimal
+    5-10 minutes: Normal
+    10-15 minutes: Delayed
+    15-30 minutes: Degraded
+    > 30 minutes: Offline
     """
     if process_block_seconds_ago is None:
         return "unknown"
     
-    if process_block_seconds_ago < 120:
+    if process_block_seconds_ago < 300:
         return "optimal"
-    elif process_block_seconds_ago < 300:
-        return "normal"
     elif process_block_seconds_ago < 600:
-        return "delayed"
+        return "normal"
     elif process_block_seconds_ago < 900:
+        return "delayed"
+    elif process_block_seconds_ago < 1800:
         return "degraded"
     else:
         return "offline"
